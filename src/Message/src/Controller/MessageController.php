e6f4ab101e22c5593611ff76ebf9af01bbce512e
adriana <adriana@rospace.com>
2017-10-16 17:34:16 +0300

    added sendMessage with ajax, needs fixes

--- src/Message/src/Controller/MessageController.php
+++ src/Message/src/Controller/MessageController.php
@@ -13,6 +13,7 @@ use Apidemia\Message\Service\MessageServiceInterface;
 use Dot\Controller\AbstractActionController;
 use Psr\Http\Message\ResponseInterface;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 
 class MessageController extends AbstractActionController
@@ -27,9 +28,10 @@ class MessageController extends AbstractActionController
         $data['post'] = $this->messageService->listMessages();
         $data['users'] = $this->messageService->getUsers();
 
-        $loggedUser = $this->authentication()->getIdentity()->getId();
-        $data['messages'] = $this->messageService->listConversations($loggedUser);
-        foreach ($data['messages'] as $message) {
+//        $loggedUser = $this->authentication()->getIdentity()->getId();
+//        $data['messages'] = $this->messageService->listConversations($loggedUser);
+//        var_dump(end($data['post'])); exit;
+        foreach ($data['post'] as $message) {
             if ($message->getIsSeen() == 0) {
                 $message->setIsSeen(1);
                 $this->messageService->sendMessage($message);
@@ -39,6 +41,30 @@ class MessageController extends AbstractActionController
     }
 
     public function createAction()
+    {
+        $data['users'] = $this->messageService->getUsers();
+
+        $senderId = $this->authentication()->getIdentity()->getId();
+
+        $createMessage = $this->getRequest()->getParsedBody();
+        $result = ['succes' => false,'message' => 'An error has ocurred'];
+
+        if (!empty($createMessage)) {
+            $storage = new MessageEntity();
+
+            $storage->setContent($createMessage['message']);
+            $storage->setSenderId($senderId);
+            $storage->setReceiverId($createMessage['receiverId']);
+            $storage->setIsSeen();
+
+            $data['post'] = $this->messageService->sendMessage($storage);
+        }
+        $result = ['succes' => 'true', 'receiverId' => $createMessage['receiverId'], 'message' => $createMessage['message']];
+
+        return new JsonResponse($result);
+    }
+
+    public function sendMessageAction()
     {
         $data['users'] = $this->messageService->getUsers();
 
